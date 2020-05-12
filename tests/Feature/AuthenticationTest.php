cc92d26ec0c8e77cc2674c06ae8fb5ced5d5f4db
yemiwebby <yemiwebby@gmail.com>
2020-05-12 09:17:06 +0100

    Check that the user is authenticated

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -7,44 +7,6 @@ use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
-    public function testMustEnterEmailAndPassword()
-    {
-        $this->json('POST', 'api/login')
-            ->assertStatus(422)
-            ->assertJson([
-                "message" => "The given data was invalid.",
-                "errors" => [
-                    'email' => ["The email field is required."],
-                    'password' => ["The password field is required."],
-                ]
-            ]);
-    }
-
-    public function testSuccessfulLogin()
-    {
-        $user = factory(User::class)->create([
-           'email' => 'sample@test.com',
-           'password' => bcrypt('sample123'),
-        ]);
-
-
-        $loginData = ['email' => 'sample@test.com', 'password' => 'sample123'];
-
-        $this->json('POST', 'api/login', $loginData, ['Accept' => 'application/json'])
-            ->assertStatus(200)
-            ->assertJsonStructure([
-               "user" => [
-                   'id',
-                   'name',
-                   'email',
-                   'email_verified_at',
-                   'created_at',
-                   'updated_at',
-               ],
-                "access_token",
-                "message"
-            ]);
-    }
 
     public function testRequiredFieldsForRegistration()
     {
@@ -82,7 +44,7 @@ class AuthenticationTest extends TestCase
     {
         $userData = [
             "name" => "John Doe",
-             "email" => "doe@example.com",
+            "email" => "doe@example.com",
             "password" => "demo12345",
             "password_confirmation" => "demo12345"
         ];
@@ -100,5 +62,46 @@ class AuthenticationTest extends TestCase
                 "access_token",
                 "message"
             ]);
+    }
+
+
+    public function testMustEnterEmailAndPassword()
+    {
+        $this->json('POST', 'api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "The given data was invalid.",
+                "errors" => [
+                    'email' => ["The email field is required."],
+                    'password' => ["The password field is required."],
+                ]
+            ]);
+    }
+
+    public function testSuccessfulLogin()
+    {
+        $user = factory(User::class)->create([
+           'email' => 'sample@test.com',
+           'password' => bcrypt('sample123'),
+        ]);
+
+
+        $loginData = ['email' => 'sample@test.com', 'password' => 'sample123'];
+
+        $this->json('POST', 'api/login', $loginData, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+               "user" => [
+                   'id',
+                   'name',
+                   'email',
+                   'email_verified_at',
+                   'created_at',
+                   'updated_at',
+               ],
+                "access_token",
+                "message"
+            ]);
+        $this->assertAuthenticated();
     }
 }
